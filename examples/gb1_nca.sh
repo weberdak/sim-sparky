cb5988d76bd6ce986d877c1aa2ddb6c95aab781b
Daniel Weber <weberdk01@gmail.com>
2019-08-21 09:02:53 -0500

    Example for NCA spectrum.

---
+++ examples/gb1_nca.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SIMSPARKY=/home/dweber/GitHub/sim-sparky
+
+python $SIMSPARKY/sim_sparky.py \
+       -i GB1_BMRB30088_CSV.dat \
+       -o gb1_nca \
+       -t csv \
+       -c CA N \
+       -s 0 \
+       --nuc1_label 13C \
+       --nuc2_label 15N \
+       --nuc1_freq 150.903 \
+       --nuc2_freq 60.834 \
+       --nuc1_center 57.5 \
+       --nuc2_center 120.00 \
+       --nuc1_sw 40.00 \
+       --nuc2_sw 40.00 \
+       --nuc1_size 1024 \
+       --nuc2_size 1024
+       
